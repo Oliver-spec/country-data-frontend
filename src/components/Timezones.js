2c63d32acc8d83467558404413164b27a4302f0a
Oliver <1155175373@link.cuhk.edu.hk>
2022-08-19 12:56:25 +0800

    add timezone conversion

--- src/components/Timezones.js
+++ src/components/Timezones.js
@@ -3,9 +3,45 @@ export default function Timezones({ country }) {
     <>
       <h3>Timezones:</h3>
       <ul>
-        {country.timezones.map((timezone, index) => (
-          <li key={index}>{timezone}</li>
-        ))}
+        {country.timezones.map((timezone, index) => {
+          // get current time in min
+          const time = new Date();
+          const timeInMin = time.getHours() * 60 + time.getMinutes();
+
+          // get user utc offset
+          const userUTCOffset = -time.getTimezoneOffset();
+
+          // get target country utc offset in min
+          const targetUTCOffset =
+            Number(timezone.substring(4, 6)) * 60 +
+            Number(timezone.substring(7));
+
+          // find the difference of time in min between user and target
+          const utcDifference =
+            timezone.substring(3, 4) === "+"
+              ? userUTCOffset - targetUTCOffset
+              : userUTCOffset + targetUTCOffset;
+
+          // find target time
+          const targetTime =
+            timeInMin - utcDifference < 0
+              ? timeInMin - utcDifference + 1440
+              : timeInMin - utcDifference > 1440
+              ? timeInMin - utcDifference - 1440
+              : timeInMin - utcDifference;
+          const targetHourString = Math.floor(targetTime / 60).toString();
+          const targetMinString =
+            (targetTime % 60).toString().length === 2
+              ? (targetTime % 60).toString()
+              : `0${targetTime % 60}`;
+
+          return (
+            <li key={index}>
+              {timezone} ={" "}
+              {`Local time: ${targetHourString}:${targetMinString}`}
+            </li>
+          );
+        })}
       </ul>
     </>
   );
